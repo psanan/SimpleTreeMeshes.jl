7a429dcd83a0f030dc65b326b7e84819241ba4d4
Patrick Sanan <patrick.sanan@gmail.com>
2023-08-02 15:47:39 +0200

    Update tests with MPI Initialization

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,12 +1,14 @@
 using Test
+using MPI
 
 push!(LOAD_PATH, "../src")
 using SimpleTreeMeshes
 
+# This test obviously does not do much!
 function test1()
   refinement_function(x, y, d, level) = level < 3
+  MPI.Init()
   tree = CreateSimpleTreeMesh(refinement_function)
-  # ..
   return true
 end
 
